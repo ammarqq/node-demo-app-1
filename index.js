3d59796491e7ab761dcd4b7bf6274f8f97d85923
ammarqq <ammarqqqq@gmail.com>
2018-10-04 17:56:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('!Hello World!2test AmmarJenkins triggerd on spinnaker');
+  res.send('!Hello World!2test tttAmmarJenkins triggerd on spinnaker');
 });
 
 var server = app.listen(3000, function () {
